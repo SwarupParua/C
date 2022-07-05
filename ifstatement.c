2130cec5e48d039deaae04ffce42daaa37d3d959
SwarupParua <108712030+SwarupParua@users.noreply.github.com>
2022-07-05 22:18:10 +0530

    Add files via upload

---
+++ ifstatement.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("The value of a is :");
+    scanf("%d",&a);
+    if(a>0){
+        printf("Posetive");
+    }
+    if(a<=0){
+        printf("non-posetive");
+    }
+}
