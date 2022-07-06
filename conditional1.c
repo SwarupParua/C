7f4d9e69b0820f84db0ab191fa4d5e23db3405d3
SwarupParua <108712030+SwarupParua@users.noreply.github.com>
2022-07-06 13:08:31 +0530

    Add files via upload

---
+++ conditional1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    /*int x;
+    printf("The value of x is:");
+    scanf("%d",x);
+    if(x%5==0){
+        printf("divisible");
+    }
+    else
+        printf("non divisible");
+    */
+
+    int x;
+    printf("The value of x is:");
+    scanf("%d",x);
+    if(x%5==0){
+        printf("divisible");
+    }
+    else
+    {
+        printf("non divisible");
+    }
+    return 0;
+}
